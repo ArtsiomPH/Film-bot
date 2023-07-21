c20066ffbc2dc94adb8c35dc09bd475a933aac7b
Artsiom_PH <workartpharm@gmail.com>
2023-07-21 18:18:20 +0300

    change credentials for render

--- config.py
+++
@@ -1,3 +0,0 @@
-TOKEN = '5527248194:AAG43YLL7TwYVGtRZgnGzjj0UiwocaSA6O0'
-APP_URL = 'https://film-list-bot.herokuapp.com/' + TOKEN
-DB_URI = 'postgres://ffspwwtnvpdocj:f83e3441743937a4edf4cf8849288aa322eb8f5708aa5af55cbf69dc47726d98@ec2-34-242-8-97.eu-west-1.compute.amazonaws.com:5432/db0ffn6if3rq96'
